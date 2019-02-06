opnsense/core

commit 5ad905c89729d8540f7056e8ea2f94988d35f8ff

Util::isPort() validation issue, closes https://github.com/opnsense/core/issues/3189

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -194,9 +194,10 @@ class Util
     {
         $tmp = explode(':', $number);
         foreach ($tmp as $port) {
-            if (!getservbyname($port, "tcp") && !getservbyname($port, "udp")
-                && filter_var($port, FILTER_VALIDATE_INT, array(
+            if (getservbyname($port, "tcp") || getservbyname($port, "udp")
+                || filter_var($port, FILTER_VALIDATE_INT, array(
                     "options" => array("min_range"=>1, "max_range"=>65535))) === false
+                || !is_numeric($port)
             ) {
                 return false;
             }

DynFi/opnsense-core

commit 38e1cd59b28cdfd7ec7c61ec5662d0f57d1035f6

rework https://github.com/opnsense/core/commit/5ad905c89729d8540f7056e8ea2f94988d35f8ff (https://github.com/opnsense/core/issues/3189), accidentally dropped support for known services ;)

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -195,8 +195,9 @@ class Util
         $tmp = explode(':', $number);
         foreach ($tmp as $port) {
             if (!getservbyname($port, "tcp") && !getservbyname($port, "udp")
-                && filter_var($port, FILTER_VALIDATE_INT, array(
-                    "options" => array("min_range"=>1, "max_range"=>65535))) === false
+                && (filter_var($port, FILTER_VALIDATE_INT, array(
+                    "options" => array("min_range"=>1, "max_range"=>65535))
+                  ) === false || !is_numeric($port))
             ) {
                 return false;
             }