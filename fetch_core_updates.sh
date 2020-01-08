01dbb530528d81f6e89c279559a4a80e5cf5e676
Dawid Kujawa <dawid.kujawa@vectus.pl>
2020-01-08 16:07:00 +0100

    Script for OPNsense core updates

--- fetch_core_updates.sh
+++ fetch_core_updates.sh
@@ -6,11 +6,11 @@ LATESTTAG=$(git ls-remote --tags opnsense | sed 's/^[^\/]*\/tags\///' | grep -e 
 echo "Latest current version: $LOCALTAG"
 echo "Latest OPNsense core version tag: $LATESTTAG"
 
-git checkout $LOCALTAL
+git checkout $LOCALTAG
 git checkout -B $LATESTTAG
 git pull --no-tags opnsense $LATESTTAG
 
 git checkout $LOCALTAG-dff-ui
-git branch -B $LATESTTAG-dff-ui
+git checkout -B $LATESTTAG-dff-ui
 
 git merge $LATESTTAG
