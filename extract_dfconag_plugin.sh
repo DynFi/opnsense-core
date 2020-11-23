b5a2d7c1ac2371c12ec508e1b9d32f97849d5b83
Dawid Kujawa <dawid.kujawa@dynfi.com>
2020-11-23 16:08:10 +0100

    Updated extract plugin script

--- extract_dfconag_plugin.sh
+++ extract_dfconag_plugin.sh
@@ -3,27 +3,27 @@
 CONFIG_PATH="$PWD/config.ini"
 source ${CONFIG_PATH}
 
-FILES="
-opnsense/service/templates/OPNsense/DFConAg/rc.conf.d
-opnsense/service/templates/OPNsense/DFConAg/+TARGETS
-opnsense/service/conf/actions.d/actions_dfconag.conf
-opnsense/scripts/dfconag/addme.py
-opnsense/scripts/dfconag/getaddoptions.sh
-opnsense/scripts/dfconag/generatekey.sh
-opnsense/scripts/dfconag/keyscan.sh
-opnsense/scripts/dfconag/reserveports.sh
-opnsense/scripts/dfconag/whoami.sh
-opnsense/scripts/dfconag/pretest.py
-opnsense/mvc/app/views/OPNsense/DFConAg/index.volt
+FILES="etc/inc/plugins.inc.d/dfconag.inc
+etc/rc.d/dfconag
+etc/rc.syshook.d/start/95-dfconag
+opnsense/mvc/app/controllers/OPNsense/DFConAg/Api/ServiceController.php
+opnsense/mvc/app/controllers/OPNsense/DFConAg/Api/SettingsController.php
+opnsense/mvc/app/controllers/OPNsense/DFConAg/IndexController.php
 opnsense/mvc/app/models/OPNsense/DFConAg/DFConAg.php
 opnsense/mvc/app/models/OPNsense/DFConAg/DFConAg.xml
-opnsense/mvc/app/controllers/OPNsense/DFConAg/Api/SettingsController.php
-opnsense/mvc/app/controllers/OPNsense/DFConAg/Api/ServiceController.php
-opnsense/mvc/app/controllers/OPNsense/DFConAg/IndexController.php
-etc/inc/plugins.inc.d/dfconag.inc
-etc/rc.syshook.d/start/95-dfconag
-etc/rc.d/dfconag
-"
+opnsense/mvc/app/views/OPNsense/DFConAg/index.volt
+opnsense/scripts/dfconag/addme.py
+opnsense/scripts/dfconag/deleteme.py
+opnsense/scripts/dfconag/disconnect.php
+opnsense/scripts/dfconag/generatekey.sh
+opnsense/scripts/dfconag/getaddoptions.sh
+opnsense/scripts/dfconag/keyscan.py
+opnsense/scripts/dfconag/pretest.py
+opnsense/scripts/dfconag/reserveports.sh
+opnsense/scripts/dfconag/whoami.sh
+opnsense/service/conf/actions.d/actions_dfconag.conf
+opnsense/service/templates/OPNsense/DFConAg/+TARGETS
+opnsense/service/templates/OPNsense/DFConAg/rc.conf.d"
 
 PWD=$(pwd)
 
