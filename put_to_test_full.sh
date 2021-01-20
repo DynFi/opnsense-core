e8523ae468fc53f46f894cb5a2b272032b1bb0bb
Dawid Kujawa <dawid.kujawa@dynfi.com>
2021-01-20 14:26:22 +0100

    Fixed bugs

--- put_to_test_full.sh
+++ put_to_test_full.sh
@@ -6,7 +6,8 @@ source ${CONFIG_PATH}
 echo "Target host: ${TARGETIP}"
 echo "SSH password: ${SSHPASS}"
 
-for F in `git diff --name-only master..connection-agent | grep -e '^src'`; do
+#for F in `git diff --name-only 20.7.8..20.7.8-dff-ui | grep -e '^src'`; do
+for F in `find src -type f`; do
     NF=`echo $F | sed 's/src/\/usr\/local/'`
     D=$(echo $NF | sed 's/[^\/]*$//')
     echo "$F >> $NF ($D)"
