opnsense/core

commit 644b647cf7b2685a8cd423f8eddc71f22240703f

IDS: "none" doesn't equal "", looks like a typo resulting in ignoring the original eve.json

src/opnsense/mvc/app/views/OPNsense/IDS/index.volt
@@ -141,7 +141,7 @@ POSSIBILITY OF SUCH DAMAGE.
             });
 
 
-            if ( selected_logfile != "") {
+            if ( selected_logfile != "none") {
                 request['fileid'] = selected_logfile;
                 request['rowCount'] = selected_max_entries;
                 request['searchPhrase'] = search_phrase;

DynFi/opnsense-core

commit 7a7ce72c5a78dfb833d1b04babb477869eb094a3

Services / Intrusion detection / Alerts - regression in https://github.com/opnsense/core/commit/644b647cf7b2685a8cd423f8eddc71f22240703f, ignoring filters and number of items to show

src/opnsense/mvc/app/views/OPNsense/IDS/index.volt
@@ -143,9 +143,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
             if ( selected_logfile != "none") {
                 request['fileid'] = selected_logfile;
-                request['rowCount'] = selected_max_entries;
-                request['searchPhrase'] = search_phrase;
             }
+            request['rowCount'] = selected_max_entries;
+            request['searchPhrase'] = search_phrase;
             return request;
         }
 