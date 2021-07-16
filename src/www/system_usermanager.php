opnsense/core

commit 18ad4dbbd3cb9e87505047e254131a454337e891

system: remove $main_buttons use #4216

src/www/system_usermanager.php
@@ -414,11 +414,6 @@ legacy_html_escape_form_data($a_user);
 
 include("head.inc");
 
-$main_buttons = array();
-if (!isset($_GET['act'])) {
-    $main_buttons[] = array('label' => gettext('Add'), 'href' => 'system_usermanager.php?act=new');
-}
-
 ?>
 <script src="<?= cache_safe('/ui/js/jquery.qrcode.js') ?>"></script>
 <script src="<?= cache_safe('/ui/js/qrcode.js') ?>"></script>
@@ -995,7 +990,32 @@ $( document ).ready(function() {
                       <th><?=gettext("Username"); ?></th>
                       <th><?=gettext("Full name"); ?></th>
                       <th><?=gettext("Groups"); ?></th>
-                      <th></th>
+                      <th class="text-nowrap">
+                        <a href="system_usermanager.php?act=new" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                          <i class="fa fa-plus fa-fw"></i>
+                        </a>
+<?php
+                        $can_import = false;
+                        if (!empty($config['system']['webgui']['authmode'])) {
+                            $servers = explode(',', $config['system']['webgui']['authmode']);
+                            foreach ($servers as $server) {
+                                $authcfg_type = auth_get_authserver($server)['type'];
+                                if ($authcfg_type == 'ldap' || $authcfg_type == 'ldap-totp') {
+                                    $can_import = true;
+                                }
+                            }
+                        }
+?>
+<?php if (!$can_import): ?>
+                        <button type="submit" name="import"
+                                id="import_ldap_users"
+                                data-toggle="tooltip"
+                                class="btn btn-primary btn-xs"
+                                title="<?= html_safe(gettext('Import')) ?>">
+                            <i class="fa fa-cloud-download fa-fw"></i>
+                        </button>
+<?php endif ?>
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -1051,29 +1071,7 @@ $( document ).ready(function() {
                           </tr>
                         </table>
                       </td>
-                      <td class="text-nowrap">
-<?php
-                        $can_import = false;
-                        if (!empty($config['system']['webgui']['authmode'])) {
-                            $servers = explode(',', $config['system']['webgui']['authmode']);
-                            foreach ($servers as $server) {
-                                $authcfg_type = auth_get_authserver($server)['type'];
-                                if ($authcfg_type == 'ldap' || $authcfg_type == 'ldap-totp') {
-                                    $can_import = true;
-                                }
-                            }
-                        }
-?>
-<?php if ($can_import): ?>
-                          <button type="submit" name="import"
-                                  id="import_ldap_users"
-                                  data-toggle="tooltip"
-                                  class="btn btn-primary btn-xs"
-                                  title="<?= html_safe(gettext('Import')) ?>">
-                              <i class="fa fa-cloud-download fa-fw"></i>
-                          </button>
-<?php endif ?>
-                      </td>
+                      <td class="text-nowrap"></td>
                     </tr>
                   </tbody>
                 </table>

src/www/system_authservers.php
@@ -328,11 +328,6 @@ legacy_html_escape_form_data($a_server);
 
 include("head.inc");
 
-$main_buttons = array();
-if (!isset($_GET['act'])) {
-    $main_buttons[] = array('label' => gettext('Add'), 'href' => 'system_authservers.php?act=new');
-}
-
 ?>
 <body>
 
@@ -896,7 +891,11 @@ else :
                   <th><?=gettext("Server Name");?></th>
                   <th style="width:25%"><?=gettext("Type");?></th>
                   <th style="width:35%"><?=gettext("Host Name");?></th>
-                  <th style="width:10%" class="text-nowrap"></th>
+                  <th style="width:10%" class="text-nowrap">
+                    <a href="system_authservers.php?act=new" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                      <i class="fa fa-plus fa-fw"></i>
+                    </a>
+                  </th>
                 </tr>
               </thead>
               <tbody>

src/www/system_camanager.php
@@ -432,14 +432,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 }
 
 legacy_html_escape_form_data($pconfig);
+
 include("head.inc");
 
-$main_buttons = array(
-    array('label' => gettext('Add'), 'href' => 'system_camanager.php?act=new'),
-);
-
 ?>
-
 <body>
   <script>
   $( document ).ready(function() {
@@ -771,7 +767,11 @@ $main_buttons = array(
               <th><?=gettext("Issuer");?></th>
               <th><?=gettext("Certificates");?></th>
               <th><?=gettext("Distinguished Name");?></th>
-              <th></th>
+              <th class="text-nowrap">
+                <a href="system_camanager.php?act=new" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                  <i class="fa fa-plus fa-fw"></i>
+                </a>
+              </th>
             </tr>
           </thead>
           <tbody>

src/www/system_certmanager.php
@@ -817,14 +817,7 @@ legacy_html_escape_form_data($a_cert);
 
 include("head.inc");
 
-if (empty($act)) {
-    $main_buttons = array(
-        array('label' => gettext('Add'), 'href' => 'system_certmanager.php?act=new'),
-    );
-}
-
 ?>
-
 <body>
   <style>
     .monospace-dialog {
@@ -2051,7 +2044,11 @@ $( document ).ready(function() {
                 <th><?=gettext("Name");?></th>
                 <th><?=gettext("Issuer");?></th>
                 <th><?=gettext("Distinguished Name");?></th>
-                <th><?=gettext("In Use");?></th>
+                <th class="text-nowrap">
+                  <a href="system_certmanager.php?act=new" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                    <i class="fa fa-plus fa-fw"></i>
+                  </a>
+                </th>
               </tr>
             </thead>
             <tbody>

src/www/system_gateway_groups.php
@@ -68,10 +68,6 @@ $service_hook = 'dpinger';
 
 include("head.inc");
 
-$main_buttons = array(
-    array('label'=> gettext('Add'), 'href'=>'system_gateway_groups_edit.php'),
-);
-
 ?>
 <script>
 $( document ).ready(function() {
@@ -122,7 +118,11 @@ $( document ).ready(function() {
                       <td><?= gettext('Name') ?></td>
                       <td><?= gettext('Gateways') ?></td>
                       <td class="hidden-xs"><?= gettext('Description') ?></td>
-                      <td class="text-nowrap"></td>
+                      <td class="text-nowrap">
+                        <a href="system_gateway_groups_edit.php" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                          <i class="fa fa-plus fa-fw"></i>
+                        </a>
+                      </td>
                     </tr>
                   </thead>
                   <tbody>
@@ -207,9 +207,10 @@ $( document ).ready(function() {
 <?php $i++;
                     endforeach; ?>
                     <tr>
-                      <td colspan="4">
+                      <td colspan="3">
                         <?= gettext("Remember to use these Gateway Groups in firewall rules in order to enable load balancing, failover, or policy-based routing. Without rules directing traffic into the Gateway Groups, they will not be used.") ?>
                       </td>
+                      <td class="text-nowrap"></td>
                     </tr>
                   </tbody>
                 </table>

src/www/system_gateways.php
@@ -198,12 +198,7 @@ $service_hook = 'dpinger';
 
 include("head.inc");
 
-$main_buttons = array(
-    array('label'=> gettext('Add'), 'href'=>'system_gateways_edit.php'),
-);
-
 ?>
-
 <script>
 $( document ).ready(function() {
   // link delete single item buttons (by class)
@@ -301,7 +296,17 @@ $( document ).ready(function() {
                     <th class="text-nowrap hidden-xs"><?= gettext('Loss') ?></th>
                     <th><?=gettext("Status"); ?></th>
                     <th class="hidden-xs hidden-sm hidden-md"><?=gettext("Description"); ?></th>
-                    <th class="text-nowrap"></th>
+                    <th class="text-nowrap">
+                      <a href="system_gateways_edit.php" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                        <i class="fa fa-plus fa-fw"></i>
+                      </a>
+<?php if (count($a_gateways)): ?>
+                          <button type="submit" id="btn_delete" name="del_x" class="btn btn-default btn-xs" data-toggle="tooltip"
+                                  title="<?= html_safe(gettext('Delete selected items')) ?>">
+                              <i class="fa fa-trash fa-fw"></i>
+                          </button>
+<?php endif ?>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -421,33 +426,6 @@ $( document ).ready(function() {
                     $i++;
                   endforeach;?>
                 </tbody>
-                <thead>
-                    <tr>
-                      <td colspan="2"></td>
-                      <td></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="text-nowrap hidden-xs"></td>
-                      <td class="text-nowrap hidden-xs"></td>
-                      <td class="text-nowrap hidden-xs"></td>
-                      <td></td>
-                      <td class="hidden-xs hidden-sm hidden-md"></td>
-                      <td class="text-nowrap">
-<?php
-                      if ($i > 0) :
-                                      ?>
-                          <button type="submit" id="btn_delete" name="del_x" class="btn btn-default btn-xs" data-toggle="tooltip"
-                                  title="<?= html_safe(gettext('Delete selected items')) ?>">
-                              <i class="fa fa-trash fa-fw"></i>
-                          </button>
-<?php
-                      endif;?>
-                      </td>
-                    </tr>
-                </thead>
               </table>
             </form>
           </div>

src/www/system_groupmanager.php
@@ -192,11 +192,6 @@ legacy_html_escape_form_data($a_group);
 
 include("head.inc");
 
-$main_buttons = array();
-if (!isset($_GET['act'])) {
-    $main_buttons[] = array('label' => gettext('Add'), 'href' => 'system_groupmanager.php?act=new');
-}
-
 ?>
 <body>
 <?php include("fbegin.inc"); ?>
@@ -414,7 +409,11 @@ $( document ).ready(function() {
                   <th><?=gettext("Group name");?></th>
                   <th><?=gettext("Member Count");?></th>
                   <th><?=gettext("Description");?></th>
-                  <th class="text-nowrap"></th>
+                  <th class="text-nowrap">
+                     <a href="system_groupmanager.php?act=new" class="btn btn-primary btn-xs" data-toggle="tooltip" title="<?= html_safe(gettext('Add')) ?>">
+                       <i class="fa fa-plus fa-fw"></i>
+                    </a>
+                  </th>
                 </tr>
               </thead>
               <tbody>
@@ -448,7 +447,7 @@ $( document ).ready(function() {
                 </tr>
 <?php endforeach ?>
                 <tr>
-                  <td colspan="4">
+                  <td colspan="3">
                     <table>
                       <tr>
                         <td></td>
@@ -461,6 +460,7 @@ $( document ).ready(function() {
                       </tr>
                     </table>
                   </td>
+                  <td class="text-nowrap"></td>
                 </tr>
               </tbody>
             </table>

DynFi/opnsense-core

commit e2fe4ce8e31b4305b92cf028e7f239d52cddad33

usermanager - missing ldap import button (regression in https://github.com/opnsense/core/commit/18ad4dbbd3cb9e87505047e254131a454337e891) cc @fichtner
(cherry picked from commit e8079f0fcf43d282a3ac92cdf98ae2eb40ea985b)

src/www/system_usermanager.php
@@ -1006,7 +1006,7 @@ $( document ).ready(function() {
                             }
                         }
 ?>
-<?php if (!$can_import): ?>
+<?php if ($can_import): ?>
                         <button type="submit" name="import"
                                 id="import_ldap_users"
                                 data-toggle="tooltip"