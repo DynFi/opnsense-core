1f54400c56880eeb0a8a80a780eb3bd06eec479b
Mariusz Zaborski <oshogbo@dynfi.com>
2021-12-20 16:45:24 +0000

    dynfi: Save version of the product
    As we are using a tar repes we have to store the current version
    of code.

--- prepare_tgz.sh
+++ prepare_tgz.sh
@@ -13,6 +13,15 @@ FD=opnsense-core-21.10.1-$V.tgz
 echo "Preparing $F"
 git archive --output=$F HEAD
 
+# Save version of the git.
+
+tmpfile=`mktemp /tmp/ver.XXXXXX`
+echo "#!/bin/sh" > "${tmpfile}"
+echo "echo \"$(./Scripts/version.sh)\"" >> "${tmpfile}"
+chmod 755 "${tmpfile}"
+tar --update --file=$F -s "|${tmpfile}|Scripts/version.sh|" --gid 0 --uid 0 "${tmpfile}"
+rm "${tmpfile}"
+
 echo "Building $FD"
 gzip -c $F > $FD
 rm $F
