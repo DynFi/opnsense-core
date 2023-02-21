b9a27dba9e0be95afc6ab7c453901acd3ab07380
Dawid Kujawa <dawid.kujawa@dynfi.com>
2023-02-21 20:02:38 +0100

    Translations updated

--- translations/makemerge.sh
+++ translations/makemerge.sh
@@ -6,7 +6,7 @@ fi
 LANGUAGE=${1}
 
 msgmerge -U -N --backup=off ${LANGUAGE}.po ${LANGUAGE}.pot
-# sed -i '' -e '/^#~.*/d' ${LANGUAGE}.po
+sed -i '/^#/d' ${LANGUAGE}.po
 
 msgfmt -o /dev/null ${LANGUAGE}.po
 echo $(grep -c ^msgid ${LANGUAGE}.po) / $(grep -c ^msgstr ${LANGUAGE}.po)
